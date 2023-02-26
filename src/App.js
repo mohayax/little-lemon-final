23cddcb78f4606b8c4edfb711cd9202a975e1097
mohayax <mohayax02@gmail.com>
2023-02-27 00:18:24 +0100

    main component started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Nav from './Components/Nav';
 import Header from './Components/Header';
+import Main from './Components/Main';
 function App() {
   return (
    <>
    <Nav/>
    <Header/>
+   <Main/>
    </>
 );
   
---
+++ src/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+  return (
+    <div>
+    <div className='main'>
+        <div className="main-head">
+        <h1>This Weeks Speacial!</h1>
+        <a href="" className='btn'>Online Menu</a>
+        </div>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Main
